e735ef7bb55ee0d85b6745a98fb99040a03d2668
Ahmed-Sokar2020 <sokar7288@gmail.com>
2026-04-14 17:33:39 +0200

    create-home-page-carousel

--- lib/data.ts
+++ lib/data.ts
@@ -33,5 +33,31 @@ const data = {
       href: "/page/help",
     },
   ],
+  carousels: [
+    {
+      title: "Most Popular Shoes For Sale",
+      buttonCaption: "Shop Now",
+      image: "/images/banner3.jpg",
+      url: "/",
+      // url: "/search?category=Shoes",
+      isPublished: true,
+    },
+    {
+      title: "Best Sellers in T-Shirts",
+      buttonCaption: "Shop Now",
+      image: "/images/banner1.jpg",
+      // url: "/search?category=T-Shirts",
+      url: "/",
+      isPublished: true,
+    },
+    {
+      title: "Best Deals on Wrist Watches",
+      buttonCaption: "See More",
+      image: "/images/banner2.jpg",
+      url: "/",
+      // url: "/search?category=Wrist Watches",
+      isPublished: true,
+    },
+  ],
 };
 export default data;
