8d318b06244f69bb2e96851f878b7a078d016e4a
Ahmed-Sokar2020 <sokar7288@gmail.com>
2026-04-14 22:27:57 +0200

    connect-to-mongoDB-and-seed-products

---
+++ lib/db/seed.ts
@@ -0,0 +1,24 @@
+import { cwd } from "process";
+import { loadEnvConfig } from "@next/env";
+import { connectToDatabase } from ".";
+import Product from "./models/product.model";
+import data from "@/lib/data";
+
+// To have access to the .env.local file here in this file
+loadEnvConfig(cwd());
+
+const main = async () => {
+  try {
+    const { products } = data;
+    await connectToDatabase(process.env.MONGODB_URI);
+    await Product.deleteMany();
+    const createdProducts = await Product.insertMany(products);
+    console.log({ createdProducts, message: "Seeded database successfully" });
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    throw new Error("Failed to seed database");
+  }
+};
+
+main();
